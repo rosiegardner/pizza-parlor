9fbe86d8deba43f2e8b89200926b98c111c3b16d
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-26 11:00:19 -0800

    WIP: update radio toppings to check box, UI functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // Business Logic for Pizza()
 
 function Pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaTopping) {
-  this.pizzaSize = pizzaSize;
-  this.pizzaCrust = pizzaCrust;
-  this.pizzaSauce = pizzaSauce;
-  this.pizzaTopping = pizzaTopping;
-  this.pizzaPricing = 15;
+  this.pizzaSize = pizzaSize; // Size of pizza - SM/MD/LG
+  this.pizzaCrust = pizzaCrust; // Style of Crust(4)
+  this.pizzaSauce = pizzaSauce; // Sauce Selection (2)
+  this.pizzaTopping = pizzaTopping; // Toppings
+  this.pizzaPricing = 15; // Base Value of Pizza (SM)
 }
 
 
@@ -15,35 +15,40 @@ Pizza.prototype.myOrder = function() {
 
 Pizza.prototype.totalOrder = function() {
   if (this.pizzaSize === "medium-13inch") {
-    this.pizzaPricing += 3;
+    this.pizzaPricing += 3; // This will increase the value by 3 for MD
   }
   if (this.pizzaSize === "large-16inch") {
-    this.pizzaPricing += 6;
+    this.pizzaPricing += 6; // This will increase the value by 6 for LG
   }
   if (this.pizzaCrust === "Thick Crust") {
-    this.pizzaPricing += 3;
+    this.pizzaPricing += 3; // This will increase the value by 3 for all Sizes
   }
   if (this.pizzaCrust === "Stuffed Crust") {
-    this.pizzaPricing += 2;
+    this.pizzaPricing += 2; // This will increase the value by 2 for all Sizes
   } 
-  if (this.pizzaTopping === " ") {
-    this.pizzaPricing += 1;
+  if (this.pizzaTopping === " ") { 
+  this.pizzaPricing += 1; // This will increase the value by 1 for all Toppings
   } else {
     this.pizzaPricing += 0;
   }
-  return this.pizzaPricing;
+  return this.pizzaPricing; // This will return the total Value of my Order
 };
 
 // User Interface Logic
 
 $(document).ready(function() {
-  $("#pizzaOrder").submit(function(event) {
+  $("form#pizzaOrder").submit(function(event) {
     event.preventDefault();
-    let pizzaSize = $("input#pizza-size").val();
-    let pizzaSauce = $("input#pizza-slime").val();
-    let pizzaCrust = $("input#pizza-crust").val();
-    let pizzaTopping = $("input#pizza-topping").val();
+    let pizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    let pizzaCrust = $("input:radio[name=pizza-crust]:checked").val();
+    let pizzaSauce = $("input:radio[name=pizza-slime]:checked").val();
+    let pizzaTopping = $("input:checkbox[name=pizza-topping]:checked").val();
+    let pizzaPricing = myPizza.totalOrder();
+    $("#myGremlinPizza").text(myPizza);
+    $("#myGremlin-hidden").show(pizzaPricing);
 
-    const myPizza = new Pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaTopping, pizzaPrice);
+
+    const myPizza = new Pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaTopping, pizzaPricing);
+    
   });
 });
